efba3f02f4e5e7c18aba434c5270415030f65509
Bedasius Budida <bedabudida@gmail.com>
2012-03-08 12:52:24 +0300

    first commit of mysql driver class

---
+++ class.MySQLDriver.php
@@ -0,0 +1,14 @@
+<?php
+	include("interface.dbdriver.php");
+	class MySQLDriver implements DBDriver {
+		public function connect() {
+			//connect to database.
+			
+		}
+		
+		public function execute($query) {
+			//execute some query here..
+		}
+	}
+
+?>
