bc24504e4425d4468d2386a31ac2fb3b0ca83f3b
Budida <bedabudida@gmail.com>
2012-03-08 12:59:32 +0300

    Update class.WordCounter.php

--- class.WordCounter.php
+++ class.WordCounter.php
@@ -2,14 +2,14 @@
 	class WordCounter {
 		const ASC  =  1;
 		const DESC =  2;
-	    private $words;
+	        private $words;
 	    
 	    function __construct($filename) {
 			$file_content = file_get_contents($filename);
 			$this->words = (array_count_values(str_word_count(strtolower($file_content), 1)));
 		}
 		
-		public function count($order) {
+	    public function count($order) {
 			if($order == self::ASC) {
 				asort($this->words);
 			}  else if($order == self::DESC) {
